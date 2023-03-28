57a367c9b5df3b1e8584e36f561de5e7b006f029
pavlovic <B.Pavlovic@printecgroup.com>
2023-03-29 00:31:58 +0200

    OK returned

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,2 +1,23 @@
-package com.example.demo;public class Main {
+package com.example.demo;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Component
+public class Main  {
+
+    @GetMapping("/health")
+
+    public String checkHealth(){
+        return "OK";
+    }
+    /*@Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        builder.up()
+                .withDetail("workingFine","OK");
+    }*/
 }
